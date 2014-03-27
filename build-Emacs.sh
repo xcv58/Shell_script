c76c5af2755b7df0218e632e40f819d6ea0a8e20
xcv58 <chenyihonglove@gmail.com>
2014-03-27 02:16:53 -0400

    add build emacs script. let it build everyday by crontab.

---
+++ build-Emacs.sh
@@ -0,0 +1,19 @@
+build() {
+    mv /usr/local/bin/gcc /usr/local/bin/gccbak
+    cd ~/emacs
+    git pull github master
+    git pull origin master
+    ./configure --with-ns
+    make install
+    if [ -d ~/emacs/nextstep/Emacs.app ]
+    then
+        if [ -d /Applications/EmacsBAK.app ]
+        then
+            rm -rf /Applications/EmacsBAK.app
+        fi
+        mv /Applications/Emacs.app /Applications/EmacsBAK.app
+        mv ~/emacs/nextstep/Emacs.app /Applications/Emacs.app
+    fi
+    mv /usr/local/bin/gccbak /usr/local/bin/gcc
+}
+build
