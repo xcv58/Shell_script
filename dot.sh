e2b6a83cee72540f02ab94d508b1c7d9c3d00d6b
xcv58 <chenyihonglove@gmail.com>
2014-04-29 00:53:24 -0400

    add dot.sh to save time when generate pdf file for .dot file.

---
+++ dot.sh
@@ -0,0 +1,15 @@
+usage() {
+    echo "Please type the dot file."
+    exit
+}
+if [[ $# == 0 ]]; then
+    usage
+fi
+origin=$1
+if [ -f $origin ]; then
+    target=${origin%.*}.pdf
+    dot $origin -Tpdf -o $target
+    open $target
+else
+    echo File $origin doesn\'t exist or has other issue.
+fi
