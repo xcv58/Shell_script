15d1a61cc02b5c67da932c9f26d00dbaad8e42fc
xcv58 <chenyihonglove@gmail.com>
2014-12-27 14:07:55 -0500

    Change pull to fetch; change email address.

--- build-Emacs.sh
+++ build-Emacs.sh
@@ -1,7 +1,6 @@
 build() {
     cd ~/emacs
-    git pull github master
-    git pull origin master
+    git fetch --all
     ./configure --with-ns
     make install
     if [ -d ~/emacs/nextstep/Emacs.app ]
@@ -20,7 +19,7 @@ sendResult() {
     curl -s --user "${apikey}" \
          https://api.mailgun.net/v2/mg.xcv58.com/messages \
          -F from='Emacs Building System <emacs.build@xcv58.com>' \
-         -F to=log@xcv58.com \
+         -F to=i@xcv58.com \
          -F subject='Emacs Build Result' \
          -F text="${result}"
 }
