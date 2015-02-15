cd7cad66e52f106d03c53768f102827a7af2c22f
xcv58 <chenyihonglove@gmail.com>
2015-02-15 00:59:18 -0500

    add build MacVim script

---
+++ build-MacVim.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+TARGET=~/macvim/src/MacVim/build/Release/MacVim.app
+BAK=/Applications/MacVimBak.app
+INSTALL=/Applications/MacVim.app
+build() {
+    cd ~/macvim/src
+    git fetch --all
+    ./configure
+    make
+    if [ -d ${TARGET} ]
+    then
+        if [ -d ${BAK} ]
+        then
+            rm -rf ${BAK}
+        fi
+        mv ${INSTALL} ${BAK}
+        mv ${TARGET} ${INSTALL}
+    fi
+}
+sendResult() {
+    result=$(tail -2 ~/.emacsbuildlog)
+    apikey=$(cat ~/.api/mailgun.apikey)
+    curl -s --user "${apikey}" \
+         https://api.mailgun.net/v2/xcv58.com/messages \
+         -F from='MacVim Building System <macvim.build@xcv58.com>' \
+         -F to=i@xcv58.com \
+         -F subject='MacVim Build Result' \
+         -F text="${result}"
+}
+build
+date
+# sendResult
