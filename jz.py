6a563cc0dcea175fbac706f582df4cbe082c2eb9
xcv58 <chenyihonglove@gmail.com>
2015-07-08 22:04:46 -0400

    change jz.py to put record to one line

--- jz.py
+++ jz.py
@@ -1,8 +1,7 @@
 #! /usr/bin/python
 import quip
-import datetime
+import time
 import sys
-import re
 from os.path import expanduser
 if len(sys.argv) <= 1:
     print "Please input arguments"
@@ -17,18 +16,13 @@ try:
     user = client.get_authenticated_user()
 
     record = " ".join(sys.argv[1:])
-    date = datetime.date.today().strftime('%Y%m%d')
+    date = time.strftime("%Y%m%d %H:%M")
 
     thread = client.get_thread(THREAD_ID)
     line = [line for line in thread["html"].split('\n') if line != ""][1]
     title_scetion_id = line.split('\'')[1]
-    m = re.search('20\d{6}', line)
-    if m and m.group(0) == date:
-        content = "<p>" + record + "</p>"
-        edit_mode = 2
-    else:
-        content = "<p class='line'>" + date + "</p><p>" + record + "</p><p> </p>"
-        edit_mode = 3
+    content = "<p>" + date + " " + record + "</p>"
+    edit_mode = 3
     client.edit_document(THREAD_ID, content, edit_mode, "html", title_scetion_id)
     print date, record
 except:
