125dcd74b7b5da8d9f7fcc9c805251f71c16f44e
xcv58 <i@xcv58.com>
2022-05-08 10:12:20 -0400

    Add cloudflared and shell history backups

--- backup-homebridge.sh
+++ backup-homebridge.sh
@@ -3,6 +3,9 @@ TARGET=~/.homebridge
 
 backup() {
     rsync -a -r pi:/var/lib/homebridge/ ${TARGET}
+    rsync -a pi:/etc/cloudflared/config.yml ${TARGET}/cloudflared/
+    rsync -a pi:/home/pi/.zhistory ${TARGET}/home-dir-pi/
+    rsync -a pi:/home/pi/.bash_history ${TARGET}/home-dir-pi/
     cd ${TARGET}
     git add .
     git commit -am "Cron backup"
@@ -12,7 +15,7 @@ sendResult() {
     apikey=$(cat ~/.api/mailgun.apikey)
     subject="Homebridge Backup Result from `hostname -s`"
     curl -s --user "${apikey}" \
-         https://api.mailgun.net/v2/xcv58.com/messages \
+         https://api.mailgun.net/v3/xcv58.com/messages \
          -F from='Homebridge Backup <homebridge.backup@xcv58.com>' \
          -F to=i@xcv58.com \
          -F subject="${subject}" \
